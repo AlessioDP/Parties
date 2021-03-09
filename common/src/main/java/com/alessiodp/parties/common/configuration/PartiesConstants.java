2b1f47486f9c497f4f17e55ba74bc0328cb8e08b
AlessioDP <alessiodptv@gmail.com>
2021-03-09 17:30:02 +0100

    Updated config versions

--- common/src/main/java/com/alessiodp/parties/common/configuration/PartiesConstants.java
+++ common/src/main/java/com/alessiodp/parties/common/configuration/PartiesConstants.java
@@ -15,11 +15,11 @@ public class PartiesConstants {
 	
 	
 	// Versions
-	public static final int VERSION_BUKKIT_CONFIG_MAIN = 11;
-	public static final int VERSION_BUKKIT_CONFIG_PARTIES = 7;
+	public static final int VERSION_BUKKIT_CONFIG_MAIN = 12;
+	public static final int VERSION_BUKKIT_CONFIG_PARTIES = 8;
 	public static final int VERSION_BUKKIT_MESSAGES = 12;
-	public static final int VERSION_BUNGEE_CONFIG_MAIN = 10;
-	public static final int VERSION_BUNGEE_CONFIG_PARTIES = 6;
+	public static final int VERSION_BUNGEE_CONFIG_MAIN = 11;
+	public static final int VERSION_BUNGEE_CONFIG_PARTIES = 7;
 	public static final int VERSION_BUNGEE_MESSAGES = 10;
 	public static final int VERSION_DATABASE_YAML = 2;
 	
